d061b07e9a85f34a61aaf720ac73d8f0ceb7734b
Viraj76 <virajg.7680@gmail.com>
2023-03-09 14:59:32 +0530

    Connected with Firebase and setting some initial git

---
+++ crudadmin/src/main/java/com/example/crudadmin/UsersData.kt
@@ -0,0 +1,10 @@
+package com.example.firebasecrud
+
+import android.accessibilityservice.GestureDescription.StrokeDescription
+
+data class UsersData(
+    val name: String?=null,
+    val operator : String?=null,
+    val location : String?=null,
+    val phone: String?= null
+)
