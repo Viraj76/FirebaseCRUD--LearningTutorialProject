8be485d50262176fa8f3e27dd39e36cd482cfb95
Viraj76 <virajg.7680@gmail.com>
2023-03-10 21:59:55 +0530

    Read Activity

--- crudadmin/src/main/java/com/example/crudadmin/ReadActivity.kt
+++ crudadmin/src/main/java/com/example/crudadmin/ReadActivity.kt
@@ -2,10 +2,45 @@ package com.example.crudadmin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.crudadmin.databinding.ActivityReadBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class ReadActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReadBinding
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_read)
+        binding = ActivityReadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.searchButton.setOnClickListener {
+            val searchPhone : String = binding.searchPhone.text.toString()
+            if  (searchPhone.isNotEmpty()){
+                readData(searchPhone)
+            }else{
+                Toast.makeText(this,"PLease enter the phone number", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+    private fun readData(phone: String) {
+        databaseReference = FirebaseDatabase.getInstance().getReference("Users")
+        databaseReference.child(phone).get().addOnSuccessListener {
+            if (it.exists()){
+                val name = it.child("name").value
+                val operator = it.child("operator").value
+                val location = it.child("location").value
+                Toast.makeText(this,"Results Found", Toast.LENGTH_SHORT).show()
+                binding.searchPhone.text.clear()
+                binding.readName.text = name.toString()
+                binding.readOperator.text = operator.toString()
+                binding.readLocation.text = location.toString()
+            }else{
+                Toast.makeText(this,"Phone number does not exist",Toast.LENGTH_SHORT).show()
+            }
+        }.addOnFailureListener{
+            Toast.makeText(this,"Something went wrong",Toast.LENGTH_SHORT).show()
+        }
     }
 }
